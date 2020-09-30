c96e1ff57740661a732aeb067865a80042a899ca
Yiming <40975373+yiming95@users.noreply.github.com>
2020-09-30 14:46:09 +0800

    Add files via upload

---
+++ src/test.py
@@ -0,0 +1,21 @@
+import numpy
+print('numpy:', numpy.__version__)
+
+import scipy
+print('scipy:', scipy.__version__)
+
+import matplotlib
+print('matplotlib:', matplotlib.__version__)
+
+import IPython
+print('iPython:', IPython.__version__)
+
+import sklearn
+print('scikit-learn:', sklearn.__version__)
+
+import keras
+print('keras: ', keras.__version__)
+
+import tensorflow as tf
+print('Tensorflow: ', tf.__version__)
+
